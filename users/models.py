8a324cebdea3f618bda3898b570ca9147ea3f937
Harry <mcclean449@gmail.com>
2024-07-19 10:27:08 +0300

    registration forms

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(
+        upload_to="users_images", blank=True, null=True, verbose_name="Аватар"
+    )
+
+
+class Meta:
+    db_table = "user"  # как отображать модель в БД
+    verbose_name = "Пользователя"  # название категории в ед.числе
+    verbose_name_plural = "Пользователи"  # название категории в мн.числе
+
+
+def __str__(self):
+    return self.username  # отображение названия queryseta
--- users/views.py
+++ users/views.py
@@ -1,20 +1,45 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.urls import reverse
 
 # Create your views here.
 
 
 def login(request):
-    context = {"title": "ArtWood - Авторизация"}
+    if request.method == 'POST': #если метод post
+        form = UserLoginForm(data=request.POST) #в форму передаем данные веденные пользователем(data)
+        if form.is_valid(): #если данные верны
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,password=password) #формируем пользователя
+            if user:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('myproject:index')) #HttpResponseRedirect перенаправляем,reverse - преобразует в  url адрес
+    else:
+        form = UserLoginForm() #если метод get() формируем пустую форму,контекст и отправляем
+
+    context = {"title": "ArtWood - Авторизация", "form":form}
     return render(request, "users/login.html", context)
 
 
 def logout(request):
-    context = {"title": "ArtWood - Выйти"}
-    return render(request, "users/logout.html", context)
+    auth.logout(request)
+    return redirect(reverse("myproject:index"))
 
 
 def registration(request):
-    context = {"title": "ArtWood - Регистрация"}
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():#если валидна регистрация
+            form.save() #данные формы сохраняются
+            user=form.instance
+            auth.login(request,user) #и сразу логинится на сайте
+            return HttpResponseRedirect(reverse('myproject:index')) #и редиректим пользователя на главную страницу
+    else:
+        form = UserRegistrationForm()
+    context = {"title": "ArtWood - Регистрация",'form':form}
     return render(request, "users/registration.html", context)
 
 
